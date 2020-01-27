ec7f968a81d1cc8670e5e76dae9d4a4b0d182a9b
PTrostowiecki <bratgollum@gmail.com>
2020-01-27 05:45:14 +0000

    Upload to git

---
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -0,0 +1,16 @@
+package com.crud.tasks.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Task {
+    private Long id;
+    private String title;
+    private String content;
+}
---
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskDto {
+    private Long id;
+    private String title;
+    private String content;
+}
